3f07d90df4c4a68614544e84f6b5dbac2dbec8cb
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2021-10-21 19:19:36 +0100

    day01

---
+++ bolum1_giris/app.js
@@ -0,0 +1,20 @@
+// * =======================================================
+// *                       KONSOL
+// * =======================================================
+// * Konsolda mesaj vermek için console.log() fonksiyonu kullanılır.
+console.log("Hello Javascript");
+console.log("Javascript javadan kolay bir dildir..");
+console.error("Bu bir hatadir");
+console.warn("Bu bir uyaridir");
+
+alert("Dikkat et Mucahit abi.."); // ? Ekrana pop-up mesaj çıkarır.
+
+// * =======================================================
+// *                 DEĞİŞKEN TANIMLAMA
+// * =======================================================
+
+//************************ VAR ****************************
+// ? var keyword ile değişken tanımlanabilir.
+// ? Ancak, modern JS var keyword'unun kullanımı azaldı.
+//? Çünkü, var global değişken gibi düşünülebilir.
+// ? Günümüzde programcılar global değişken gerekmedikçe VAR tercih etmemektedir.
