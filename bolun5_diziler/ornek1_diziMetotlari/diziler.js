7908e87498b3876608b453c0e4b68c99dbdd6419
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2021-12-01 18:19:27 +0000

    day12

--- bolun5_diziler/ornek1_diziMetotlari/diziler.js
+++ bolun5_diziler/ornek1_diziMetotlari/diziler.js
@@ -7,9 +7,43 @@ const isimler = [
   "Mucahit",
   20,
   50,
+  false,
   true,
   22.5,
   2021 - dogum,
   yaslar,
 ];
 console.log(isimler);
+//TODO ========= DİZİYİ DEĞİŞTİREN METOTLAR ==============
+const h1 = document.getElementById("meyveler");
+let meyveler = ["Elma", "Armut", "Muz", "Nar", "Kivi"];
+console.log(meyveler);
+//!pop() -> son elemani siler ve silinen diziyi dondurur
+meyveler.pop(); // ['Elma', 'Armut', 'Muz', 'Nar']
+console.log(meyveler);
+
+//!push() -> dIZININ SONUNA BIR YADA DAHA FAZLA ELEMAN ekler ve dizinin son eleman sayisini dondurur.
+const manav = meyveler.push("Cilek", "Karpuz");
+console.log(manav);
+//! reverse() -> Dizinin tamamini ters cevirir
+meyveler.reverse();
+console.log(meyveler);
+//! shift() -> ilk elemani siler ve bu haliyle diziyi dondurur.
+meyveler.shift();
+console.log(meyveler);
+//! unshift() -> ilk eleman ekler ve bu haliyle diziyi dondurur
+meyveler.unshift("Mongo");
+console.log(meyveler);
+//! sort() -> diziyi alfabetik olarak siralar
+meyveler.sort();
+console.log(meyveler);
+meyveler.splice();
+
+//TODO ========= DİZIYE ERISIM METOTLARI ==============
+
+const sayilar = [3, 5, 2, "2", "iki", 2, "bes", 5];
+console.log(sayilar.includes("bes"));//true
+console.log(sayilar.includes(5)); //true
+console.log(sayilar.includes(10));//false
+
+
